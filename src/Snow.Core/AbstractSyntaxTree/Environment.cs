0fc2bfe45b1ee8d286f0129c373565ac2c548485
Wessel van Lit <wessel@litdev.nl>
2022-09-25 22:09:14 +0200

    Make missing definition error more clear

--- src/Snow.Core/AbstractSyntaxTree/Environment.cs
+++ src/Snow.Core/AbstractSyntaxTree/Environment.cs
@@ -9,4 +9,20 @@ public class Environment : Dictionary<string, AstExpression>
     public Environment(Environment env) : base(env)
     {
     }
+
+    public new AstExpression this[string key]
+    {
+        get
+        {
+            try
+            {
+                return base[key];
+            }
+            catch (KeyNotFoundException keyNotFound)
+            {
+                throw new Exception($"'{key}' is not defined current scope");
+            }
+        }
+        set => base[key] = value;
+    }
 }
